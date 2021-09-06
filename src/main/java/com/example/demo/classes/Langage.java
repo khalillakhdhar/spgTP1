d90c53004b9c5b7ad2c9e27d368ab9ce98476b8d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-06 10:27:23 +0100

    Create Langage.java

---
+++ src/main/java/com/example/demo/classes/Langage.java
@@ -0,0 +1,33 @@
+package com.example.demo.classes;
+
+public class Langage {
+private int id;
+private String titre;
+private String domaine;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getTitre() {
+	return titre;
+}
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+public String getDomaine() {
+	return domaine;
+}
+public void setDomaine(String domaine) {
+	this.domaine = domaine;
+}
+@Override
+public String toString() {
+	return "Langage [id=" + id + ", titre=" + titre + ", domaine=" + domaine + "]";
+}
+
+
+
+
+}
